335076d2864785e71417874b22126ed561adf27c
Fitri <fitrialrahim@gmail.com>
2025-03-28 15:01:54 +0800

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -6,7 +6,7 @@ echo "Sourcing virtual env"
 source /workspace/venv/bin/activate
 
 echo "Starting WebUI API"
-python /workspace/stable-diffusion-models/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python /workspace/stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python -u /handler.py
